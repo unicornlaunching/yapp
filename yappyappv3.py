309cb98815e524ac2a81e93029a8e24a7478ac78
unicornlaunching <45397093+unicornlaunching@users.noreply.github.com>
2023-11-09 21:39:35 -0500

    Update yappyappv3.py

--- yappyappv3.py
+++ yappyappv3.py
@@ -1,5 +1,8 @@
 import streamlit as st
+from pytube import YouTube
 from youtube_transcript_api import YouTubeTranscriptApi
+from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
+import os
 
 def get_transcript(video_url):
     if 'youtu.be' in video_url:
@@ -15,9 +18,26 @@ def get_transcript(video_url):
         st.write('Error retrieving transcript: ', str(e))
         return None
 
-    # Filter only the transcribed text
     texts = [d.get('text') for d in transcript]
     return " ".join(texts)
+
+def download_clip(video_url):
+    if 'youtu.be' in video_url:
+        video_id = video_url.split('.be/')[1]
+        video_id = video_id.split('?')[0]
+        video_url = f"https://www.youtube.com/watch?v={video_id}"
+
+    # Download video
+    yt = YouTube(video_url)
+    stream = yt.streams.first()
+    stream.download(filename="temp")
+
+    # Cut first 10 seconds
+    ffmpeg_extract_subclip("temp.mp4", 0, 10, targetname="clip.mp4")
+
+    # Remove the temp file
+    if os.path.exists("temp.mp4"):
+        os.remove("temp.mp4")
 
 st.title('YouTube Transcript Extraction')
 
@@ -28,5 +48,12 @@ if st.button('Get Transcript'):
         result = get_transcript(video_url)
         if result:
             st.write(result)
+    else:
+        st.write("Please enter a valid URL")
+
+if st.button('Download 10-second Clip'):
+    if video_url:
+        download_clip(video_url)
+        st.write("Download Completed. Check the app's directory for 'clip.mp4'")
     else:
         st.write("Please enter a valid URL")
